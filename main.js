033ecda1fea6893572f5c3577e86ae1b92ddf547
D.Tamilarasan <abidm26791@gmail.com>
2022-12-15 23:00:56 -0800

    js

---
+++ main.js
@@ -0,0 +1,10 @@
+var a,b,c;
+yourname=prompt("Enter Your Name");
+p=prompt("How much time your name is print");
+function swap(yourname,p){
+for(i=0;i<p;i++){
+document.write(yourname+"<br>");
+}
+
+}
+swap(yourname,p)
